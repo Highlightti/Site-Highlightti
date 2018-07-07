707ab69cdf096f7c36f7ec7d52816d4cb9b0e8d8
Francisco Junior <franciscov208@gmail.com>
2018-07-07 14:30:04 -0300

    Site Atual

---
+++ js/arrow.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function ($) {
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 100,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 200,
+		//grab the "back to top" link
+		$back_to_top = $('#arrow');
+
+
+
+	//hide or show the "back to top" link
+	$(window).scroll(function () {
+		($(this).scrollTop() > offset) ? $back_to_top.removeClass('arrowfadeout') : $back_to_top.addClass('arrowfadeout');
+		if ($(this).scrollTop() > offset_opacity) {
+			$back_to_top.addClass('');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function (event) {
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0,
+		}, scroll_top_duration
+		);
+	});
+
+});
